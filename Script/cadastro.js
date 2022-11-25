a9e1ea38a8dd556a713766b044ea0349563eedd1
Lucas Chagas Magno <109705062+lucascmagno@users.noreply.github.com>
2022-11-24 22:05:33 -0300

    PopUp de cadastro criado

---
+++ Script/cadastro.js
@@ -0,0 +1,8 @@
+function form(){
+    var form = document.getElementById('form');
+    if(form.style.display == 'none'){
+        form.style.display = 'block'
+    }else{
+        form.style.display = 'none'
+    }
+}
